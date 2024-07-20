b763341a0eaf2065dc8785b0d48b0cad94d94b2e
Vellanki Madhuri <116187301+madhuri1818@users.noreply.github.com>
2024-07-20 20:25:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,40 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+// const authRoutes = require('./routes/auth');
+
+// app.use('/api/auth', authRoutes);
+
+// mongoose.connect('mongodb://127.0.0.1:27017/Movie', { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
+
+// const PORT = 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config(); 
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
-const authRoutes = require('./routes/auth');
 
+const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/Movie', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+const mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Hey,Your MongoDB connected'))
   .catch(err => console.error(err));
 
 const PORT = 5000;
